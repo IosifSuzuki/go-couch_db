caf6a82d4edc84aad8cfea67bc6184660e11657d
Bogdan Petkanych <iosifsuzuki@gmail.com>
2023-09-20 01:11:52 +0300

    - fix: crash when close nil body
    - feat: add create db operation

--- connection/operation.go
+++ connection/operation.go
@@ -14,15 +14,20 @@ type Operation interface {
 	Find(model any, selector string) error
 	ExecuteView(designDoc, view string, target any, params model.QueryViewParams) error
 	CreateView(designDoc, definition string) error
+	CreateDB(db string) (bool, error)
 }
 
 func (c *connection) Ping() (bool, error) {
 	path := c.pathBuilder.Build()
 	resp, err := c.httpClient.Execute(http.MethodHead, path, nil)
-	defer resp.Body.Close()
 	if err != nil {
 		return false, err
 	}
+	defer func() {
+		if resp.Body != nil {
+			_ = resp.Body.Close()
+		}
+	}()
 	if !tool.IsSuccessStatusCode(resp.StatusCode) {
 		var respErr = model.ErrorResponse{
 			Code: resp.StatusCode,
@@ -38,10 +43,14 @@ func (c *connection) Ping() (bool, error) {
 func (c *connection) Len() (int, error) {
 	path := c.pathBuilder.AddSegment(c.db).Build()
 	resp, err := c.httpClient.Execute(http.MethodGet, path, nil)
-	defer resp.Body.Close()
 	if err != nil {
 		return 0, err
 	}
+	defer func() {
+		if resp.Body != nil {
+			_ = resp.Body.Close()
+		}
+	}()
 	if !tool.IsSuccessStatusCode(resp.StatusCode) {
 		var respErr = model.ErrorResponse{
 			Code: resp.StatusCode,
@@ -62,10 +71,14 @@ func (c *connection) Find(target any, selector string) error {
 	path := c.pathBuilder.AddSegment(c.db).AddSegment("_find").Build()
 	reader := bytes.NewReader([]byte(selector))
 	resp, err := c.httpClient.Execute(http.MethodPost, path, reader)
-	defer resp.Body.Close()
 	if err != nil {
 		return err
 	}
+	defer func() {
+		if resp.Body != nil {
+			_ = resp.Body.Close()
+		}
+	}()
 	var findResp model.FindResponse
 	if err := json.NewDecoder(resp.Body).Decode(&findResp); err != nil {
 		return err
@@ -87,6 +100,11 @@ func (c *connection) CreateView(designDoc, definition string) error {
 	if err != nil {
 		return err
 	}
+	defer func() {
+		if resp.Body != nil {
+			_ = resp.Body.Close()
+		}
+	}()
 	if !tool.IsSuccessStatusCode(resp.StatusCode) {
 		var respErr = model.ErrorResponse{
 			Code: resp.StatusCode,
@@ -108,7 +126,11 @@ func (c *connection) ExecuteView(designDoc, view string, target any, params mode
 		AddQueryModel(params).
 		Build()
 	resp, err := c.httpClient.Execute(http.MethodGet, path, nil)
-	defer resp.Body.Close()
+	defer func() {
+		if resp.Body != nil {
+			_ = resp.Body.Close()
+		}
+	}()
 	if err != nil {
 		return err
 	}
@@ -130,4 +152,27 @@ func (c *connection) ExecuteView(designDoc, view string, target any, params mode
 		return err
 	}
 	return json.NewDecoder(bytes.NewReader(docsBytes)).Decode(&target)
+}
+
+func (c *connection) CreateDB(db string) (bool, error) {
+	path := c.pathBuilder.AddSegment(c.db).Build()
+	resp, err := c.httpClient.Execute(http.MethodPut, path, nil)
+	if err != nil {
+		return false, err
+	}
+	defer func() {
+		if resp.Body != nil {
+			_ = resp.Body.Close()
+		}
+	}()
+	if !tool.IsSuccessStatusCode(resp.StatusCode) {
+		var respErr = model.ErrorResponse{
+			Code: resp.StatusCode,
+		}
+		if err := json.NewDecoder(resp.Body).Decode(&respErr); err != nil {
+			return false, err
+		}
+		return false, respErr
+	}
+	return resp.StatusCode == http.StatusCreated, nil
 }
